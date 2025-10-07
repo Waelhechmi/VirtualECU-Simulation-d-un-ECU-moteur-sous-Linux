3a7e8fde0ee1c5bb790351a11a18912521b5034f
Hechmi_wael <waelhechmi100@gmail.com>
2025-10-07 20:18:10 +0100

    update xith schudular tasks priority

--- ECU_Sim/src/actuators.c
+++
@@ -1,7 +0,0 @@
-#include "actuators.h"
-#include <stdio.h>
-
-void apply_actuators(ActuatorData a) {
-    printf("Injector=%.2f ms, Throttle=%.2f%%, Ignition=%d\n",
-           a.injector_ms, a.throttle_target, a.ignition_on);
-}
